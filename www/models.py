d9e9bd4261db56c051717559248824d03e3ea833
Bai Dong <bai123donggood@163.com>
2015-05-22 15:16:34 +0800

    add models.py

---
+++ www/models.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 
+__author__ = 'Jack Bai'
+
+'''
+Models for user, blog, comment.
+
+日期和时间用float类型存储在数据库中，而不是datetime类型，这么做的好处是:
+	不必关心数据库的时区以及时区转换问题，排序非常简单，显示的时候，只需要做一个float到str的转换，也非常容易。
+'''
+
+import time, uuid
+
+from transwarp.db import next_id
+from transwarp.orm import Model, StringField, BooleanField, FloatField, TextField
+
+
+class User(Model):
+	__table__ = 'users'
+
+	id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+	email = StringField(updatable=False, ddl='varchar(50)')
+	password = StringField(ddl='varchar(50)')
+	admin = BooleanField()
+	name = StringField(ddl='varchar(50)')
+	image = StringField(ddl='varchar(500)')
+	created_at = FloatField(updatable=False, default=time.time)
+
+
+class Blog(Model):
+	__table__ = 'blogs'
+	id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+	user_id = StringField(updatable=False, ddl='varchar(50)')
+	user_name = StringField(ddl='varchar(50)')
+	user_image = StringField(ddl='varchar(500)')
+	name = StringField(ddl='varchar(50)')
+	summary = StringField(ddl='varchar(200)')
+	content = TextField()
+	created_at = FloatField(updatable=False, default=time.time)
+
+
+class Comment(Model):
+	__table__ = 'comments'
+
+	id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+	blog_id = StringField(updatable=False, ddl='varchar(50)')
+	user_id = StringField(updatable=False, ddl='varchar(50)')
+	user_name = StringField(ddl='varchar(50)')
+	user_image = StringField(ddl='varchar(500)')
+	content = TextField()
+	created_at = FloatField(updatable=False, default=time.time)
