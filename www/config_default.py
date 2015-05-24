eb5523bf316b8b8c85b5385ba31528d62a51c6b2
Bai Dong <bai123donggood@163.com>
2015-05-24 20:02:02 +0800

    add configurations.

---
+++ www/config_default.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Jack Bai'
+
+'''
+Default Configurations.
+'''
+
+configs = {
+	'db': {
+		'host': '127.0.0.1',
+		'port': 3306,
+		'user': 'www-data',
+		'database': 'awesome'
+	},
+	'session': {
+		'secret': 'AwEsOmE'
+	}
+}
---
+++ www/config_override.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Jack Bai'
+
+'''
+Override Configuration.
+'''
+
+configs = {
+	'db': {
+		'host': '127.0.0.1'
+	}
+}
