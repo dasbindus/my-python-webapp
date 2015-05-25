96df834988d9274400aa152406f1ef951d055919
Bai Dong <bai123donggood@163.com>
2015-05-25 15:06:35 +0800

    add route(/)==>blogs.html to urls.py

--- www/urls.py
+++ www/urls.py
@@ -3,14 +3,23 @@
 
 __author__ = 'Jack Bai'
 
-import logging
+import logging, os, re, time, base64, hashlib
 
-from transwarp.web import get, view
+from transwarp.web import get, post, ctx, view, interceptor, seeother, notfound
 
 from models import User, Blog, Comment
 
-@view('test_users.html')
+
+@view('blogs.html')
 @get('/')
+def index():
+	blogs = Blog.find_all()
+	user = User.find_first('where email=?', 'admin@example.com')
+	return dict(blogs=blogs, user=user)
+
+
+@view('test_users.html')
+@get('/test_users')
 def test_users():
 	users = User.find_all()
 	return dict(users=users)
