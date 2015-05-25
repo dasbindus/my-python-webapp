14f9a8aef4d4339f4e3b57901451778d172682c5
Bai Dong <bai123donggood@163.com>
2015-05-25 15:44:45 +0800

    add datetime filter.

--- www/wsgiapp.py
+++ www/wsgiapp.py
@@ -8,13 +8,28 @@ A WSGI application entry.
 '''
 
 import logging; logging.basicConfig(level=logging.INFO)
-
-import os
+import os, time
+from datetime import datetime
 
 from transwarp import db
 from transwarp.web import WSGIApplication, Jinja2TemplateEngine
 
 from config import configs
+
+
+def datetime_filter(t):
+	delta = int(time.time() - t)
+	if delta < 60:
+		return u'1 minutes ago'
+	if delta < 3600:
+		return u'%s minutes ago' % (delta // 60)
+	if delta < 86400:
+		return u'%s hours ago' % (delta // 3600)
+	if delta < 604800:
+		return u'%s days ago' % (delta // 86400)
+	dt = datetime.fromtimestamp(t)
+	return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
+
 
 # init db(初始化数据库):
 db.create_engine(**configs.db)
@@ -24,6 +39,7 @@ wsgi = WSGIApplication(os.path.dirname(os.path.abspath(__file__)))
 
 # 初始化jinja2模板引擎:
 template_engine = Jinja2TemplateEngine(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'))
+template_engine.add_filter('datetime', datetime_filter)
 
 wsgi.template_engine = template_engine
 
@@ -34,4 +50,6 @@ wsgi.add_module(urls)
 
 # 在9000端口上启动本地测试服务器:
 if __name__ == '__main__':
-	wsgi.run(9000)
+	wsgi.run(9000, host='0.0.0.0')
+else:
+	application = wsgi.get_wsgi_application()
