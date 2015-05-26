37dbaec83ce54e094bd9f0f5e68e716aa9bed52a
Bai Dong <bai123donggood@163.com>
2015-05-26 14:41:52 +0800

    add api support.

--- www/urls.py
+++ www/urls.py
@@ -9,6 +9,8 @@ from transwarp.web import get, post, ctx, view, interceptor, seeother, notfound
 
 from models import User, Blog, Comment
 
+from apis import api, APIError, APIValueError, APIPermissionError, APIResourceNotFoundError
+
 
 @view('blogs.html')
 @get('/')
@@ -22,4 +24,14 @@ def index():
 @get('/test_users')
 def test_users():
 	users = User.find_all()
+	return dict(users=users)
+
+
+@api
+@get('/api/users')
+def api_get_users():
+	users = User.find_by('order by created_at desc')
+	for u in users:
+		# replace password with '******'
+		u.password = '******'
 	return dict(users=users)
