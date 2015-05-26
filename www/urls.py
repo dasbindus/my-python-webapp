9d23721d1b7bdff10a6f019314ffa57cdea58d6f
Bai Dong <bai123donggood@163.com>
2015-05-26 20:29:06 +0800

    update the html file.

--- www/urls.py
+++ www/urls.py
@@ -119,6 +119,33 @@ _RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$'
 _RE_MD5 = re.compile(r'^[0-9a-f]{32}$')
 
 
+@api
+@post('/api/users')
+def register_user():
+	i = ctx.request.input(name='', email='', password='')
+	name = i.name.strip()
+	email = i.email.strip().lower()
+	password = i.password
+	if not name:
+		raise APIValueError('name')
+	if not email or not _RE_EMAIL.match(email):
+		raise APIValueError('email')
+	if not password or not _RE_MD5.match(password):
+		raise APIValueError('password')
+	user = User.find_first('where email=?', email)
+	if user:
+		raise APIError('register:failed', 'email', 'Email is already in use.')
+	user = User(name=name, email=email, password=password, image='http://www.gravatar.com/avatar/%s?d=mm&s=120' % hashlib.md5(email).hexdigest())
+	user.insert()
+	# make session cookie
+	cookie = make_signed_cookie(user.id, user.password, None)
+	ctx.response.set_cookie(_COOKIE_NAME, cookie)
+	return user
+
+@view('register.html')
+@get('/register')
+def register():
+	return dict()
 
 
 @api
